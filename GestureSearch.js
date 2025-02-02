f7ad58ae11a53bfc635a7c67c10d108448179a42
suresh <125141865+suresh113322104143@users.noreply.github.com>
2025-02-02 12:29:35 +0530

    Create GestureSearch.js

---
+++ GestureSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GestureSearch() {
+  return (
+    <div className="gesture-search">
+      <p>Gesture search is in development. Stay tuned!</p>
+    </div>
+  );
+}
+
+export default GestureSearch;
